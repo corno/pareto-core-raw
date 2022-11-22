147b55665221a64254a536040031be8eefc87529
corno <corno.astn.a@schraverus.com>
2022-11-22 12:57:56 +0100

    .

--- test/src/dependencies/dependencies.p.ts
+++ test/src/dependencies/dependencies.p.ts
@@ -1,8 +1,3 @@
-
-import * as bool from "res-pareto-boolean"
-
-import { DDependencies } from "../interface"
-
-export const dependencies: DDependencies = {
-    equal: bool.f_equal
+export const dependencies = {
+    
 }
--- test/src/interface/functions/functions.p.ts
+++ test/src/interface/functions/functions.p.ts
@@ -1,4 +1,7 @@
-import { DDependencies } from "../dependencies/dependencies.p"
 import * as test from "lib-pareto-test"
+import { DDependencies } from "../dependencies/dependencies.p"
 
-export type FCreateGetTestset = ($d: DDependencies) => test.GetTestSet
+export type FCreateGetTestset = (
+    $: null,
+    $d: DDependencies,
+) => test.FGetTestSet
